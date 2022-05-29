21f311e33db2eccd224bb59d439a789e492d1376
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2022-05-29 12:19:57 +0900

    fix: useEffect 두 번 실행되는 이슈 해결

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <>
         <GlobalStyles />
         <CustomHelmet />
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>,
+    </>,
 );
